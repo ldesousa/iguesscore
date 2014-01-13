3d68d7e225fd2d1cec225f4a863c8a5154644771
Luís de Sousa <luis.a.de.sousa@gmail.com>
2014-01-13 16:48:52 +0100

    Initialised Rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iguesscore::Application.config.secret_token = 'b5752c035cf46e2cf175ba3a3c4835f7bf14a739e36881289b94ea96b432651e5e6c11683ecce2eac151b80759c6ad0d3cb329cea69820e8dc5ddbf5bce4ac01'
